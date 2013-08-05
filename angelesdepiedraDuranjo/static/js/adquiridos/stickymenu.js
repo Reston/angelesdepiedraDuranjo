527ae481d42b2c8f525574644c0c82300fa8a4b7
José Ordoñez <restonjd@gmail.com>
2013-08-05 17:40:31 -0430

    stickymenu and logo fluido

--- angelesdepiedraDuranjo/static/js/adquiridos/stickymenu.js
+++ angelesdepiedraDuranjo/static/js/adquiridos/stickymenu.js
@@ -4,39 +4,40 @@ $( document ).ready(function() {
 	posicionarMenu();
 
 	$(window).scroll(function(){
+		console.log("testing");
 		posicionarMenu();
 	});
 
 	function posicionarMenu() {
-		var altura_del_menu = $('.barraazul').outerHeight(true);
-
-		if ($(window).scrollTop()){
-			if ($(window).scrollTop() >= 80){
-				$('.logo').addClass('smaller');
+			if ($(window).scrollTop() >= altura_del_header){
+				$('.logo').stop().animate({
+							width: "180",
+							height: "94",
+							}, 'fast');
+			} else {
+				$('.logo').stop().animate({
+							width: "310",
+							height: "229",
+							}, 'fast');
 			}
-			$('.barraazul').addClass('fixed');
-			$('.contenido').css('margin-top', (altura_del_menu) + 'px');
-		} else {
-			$('.menuServicios').removeClass('fixed');
-			$('.contenido').css('margin-top', '0');
-			$('.logo').removeClass('smaller');
-		}
 	}
 
-	// This is a functions that scrolls to #{blah}link
-	function goToByScroll(id){
-	// Remove "link" from the ID
-		id = id.replace("Menu", "");
-	// Scroll
-		$('html,body').animate({
-			scrollTop: $("#"+id).offset().top},
-			'slow');
-	}
-
+	
 	$("#menuSlow > ul > li").click(function(e){
 	// Prevent a page reload when a link is pressed
 		e.preventDefault();
 	// Call the scroll function
 		goToByScroll($(this).attr("id"));
 	});
+
+	// This is a functions that scrolls to #{blah}link
+	function goToByScroll(id){
+	// Remove "link" from the ID
+	console.log(id);
+		id = id.replace("Menu", "");
+	// Scroll
+		$('html,body').stop().animate({
+			scrollTop: $("#"+id).offset().top},
+			'slow');
+	}
 });
